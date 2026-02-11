333fc848c87dfaedd99540e04322a4c884c4925e
shalva tsigroshvili <shtsigroshvili27@gmail.com>
2026-02-11 05:51:32 +0400

    fd

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {     
-               Console.WriteLine("Hello, WorlASdfDDad!");
+               Console.WriteLine("Hefdfllo, WorlASdfDDad!");
         }
     }
 }
