a8dc372716c6f4efaa6760e331a2703b7a6a8094
shalva tsigroshvili <shtsigroshvili27@gmail.com>
2026-02-11 05:47:25 +0400

    f

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {     
-               Console.WriteLine("Hello, WorlASDDad!");
+               Console.WriteLine("Hello, WorlASdfDDad!");
         }
     }
 }
